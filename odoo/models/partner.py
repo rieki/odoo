b6fed1cc10c186de8f3c89e5b92042d300592f83
root <root@yanna-dev.localdomain>
2017-05-11 22:48:27 +0000

    Update

--- odoo/models/partner.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class Partner(models.Model):
-   _inherit = 'res.partner'
-
-   credit_limit = fields.Monetary(string="Credit Limit", default=0)
