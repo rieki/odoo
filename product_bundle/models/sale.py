4cb175069bc44be3cd5b5d1d879d08c91c21e852
root <root@yanna-dev.localdomain>
2017-06-25 02:33:03 +0000

    Product bundle delivered

--- product_bundle/models/sale.py
+++ product_bundle/models/sale.py
@@ -7,7 +7,7 @@ from odoo.exceptions import UserError
 from odoo.tools import float_is_zero, float_compare, DEFAULT_SERVER_DATETIME_FORMAT
 from odoo.tools.misc import formatLang
 
-class SaleOrder(models.Model):
+class SaleOrders(models.Model):
     _inherit = 'sale.order.line'
     
     @api.multi
@@ -30,11 +30,11 @@ class SaleOrder(models.Model):
             if not line.order_id.procurement_group_id:
                 vals = line.order_id._prepare_procurement_group()
                 line.order_id.procurement_group_id = self.env["procurement.group"].create(vals)
-            if line.product_id.bundle_okay == True:
+            if line.product_id.bundle_okay == True: 
                 for bundle in line.product_id.product_bundle_list:
                     vals = line._prepare_order_line_procurement(group_id=line.order_id.procurement_group_id.id)
                     vals['product_id'] = bundle.product_id.id
-                    vals['product_qty'] = bundle.product_uom_qty - qty
+                    vals['product_qty'] = (bundle.product_uom_qty - qty) * line.product_uom_qty
                     vals['product_uom'] = bundle.product_uom.id
                     new_proc = self.env["procurement.order"].with_context(procurement_autorun_defer=True).create(vals)
                     new_proc.message_post_with_view('mail.message_origin_link',
@@ -51,3 +51,44 @@ class SaleOrder(models.Model):
                 new_procs += new_proc
         new_procs.run()
         return new_procs
+
+    @api.onchange('product_uom_qty', 'product_uom', 'route_id')
+    def _onchange_product_id_check_availability(self):
+        err = 0
+        message_store = []
+        message ="Insufficient stock for the following: \n"
+        if not self.product_id or not self.product_uom_qty or not self.product_uom:
+            self.product_packaging = False
+            return {}
+        if self.product_id.bundle_okay == True:
+            for line in self.product_id.product_bundle_list:
+                precision = line.env['decimal.precision'].precision_get('Product Unit of Measure')
+                product_qty = line.product_uom._compute_quantity(self.product_uom_qty, self.product_id.uom_id)
+                if float_compare(line.product_id.virtual_available, product_qty, precision_digits=precision) == -1:
+                    is_available = self._check_routing()
+                    if not is_available:
+                        err = err + 1
+                        title = _('Not enough inventory!')
+                        message_store = _('-Bundle contains %s %s of %s but you only have %s %s available!\nThe stock on hand is %s %s.') % \
+                                (line.product_uom_qty, line.product_uom.name, line.product_id.name,line.product_id.virtual_available, line.product_id.uom_id.name, line.product_id.qty_available, line.product_id.uom_id.name)
+                        message += message_store + "\n"
+
+            if err != 0:
+                warning_mess={
+                'title': title,
+                'message': message
+                }
+                return {'warning': warning_mess}
+            else:
+                return{}
+        else:
+            return super(SaleOrders, self)._onchange_product_id_check_availability()  
+
+
+
+
+
+
+
+
+
--- product_bundle/models/stock.py
+++ product_bundle/models/stock.py
@@ -4,23 +4,16 @@
 from odoo import api, models
 from odoo.tools import float_round
 
-class ReportDelivery(models.AbstractModels):
-    name = "report.stock.report_deliveryslip"
+class StockMove(models.Model):
+    _inherit = "stock.move"
 
+    @api.multi
+    def action_done(self):
+        result = super(StockMove, self).action_done()
 
-    @api.model
-    def render_html(self, docids, data=None):
-        #stock = self.env['stock.picking'].browse(docids)
-        data = dict(data or {})
-        #stock.orogin = 'Hello'
-        docs.update(self.origin('hello'))
-        report_obj = self.env['report']
-        report = report_obj._get_report_from_name('stock.report_deliveryslip')
-        docargs = {
-            'doc_ids': data.get('ids', data.get('active_ids')),
-            'doc_model': 'stock.picking',
-            'docs': stock,
-        }
-    
-        return report_obj.render('stock.report_deliveryslip', docargs)
-
+        # Update delivered quantities on sale order lines
+        sale_order_lines = self.filtered(lambda move: move.procurement_id.sale_line_id and move.product_id.expense_policy == 'no').mapped('procurement_id.sale_line_id')
+        for line in sale_order_lines:
+            if sale_order_lines.product_id.bundle_okay == True:
+            line.qty_delivered = line._get_delivered_qty()
+        return result
